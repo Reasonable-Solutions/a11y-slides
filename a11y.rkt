3cd0cb376a80da92d836670a3ba8852ea3baf869
Carl Hedgren <carl.hedgren@nav.no>
2023-11-20 12:00:32 +0100

    text updates

--- a11y.rkt
+++ a11y.rkt
@@ -9,17 +9,11 @@
 
 (define uu-katt (svg-file->pict katt.svg 0.2))
 
-; this should be some nice nais colored top bar with a logo
-(define top-bar 5)
-; and a nice bottom bar
-(define bottom-bar 5)
-
-
 (define (ltt body-text)
   (para #:align 'left (tt body-text)))
 
 
-(slide #:title "Teknisk demo: Tilgjengelighetserklæring"
+(slide #:title "Teknisk demo: Hvordan lage Tilgjengelighetserklæring"
        uu-katt
        (t "Carl Hedgren")
        (t "Rannveig A. Skjerve")
@@ -30,7 +24,9 @@
        )
        (t "Det innebærer et rapporteringskrav fra vår side")
        'next
-       (t "Via et grensesnitt med en webportal (Du logger in med organisasjonsnummer)")
+       (t "Via et grensesnitt med en webportal")
+       (t "(Du logger in med organisasjonsnummer)")
+       'next 
        (t "Det blir fort _veldig jobbigt_"))
 
 (slide #:title "Vi laget et verktøy"
